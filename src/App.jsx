c9ae1ff93c9e927d09398f20395721b6fa0ddd64
John Carlo Bartolome <engrjohncarlobartolome@gmail.com>
2025-04-05 22:32:16 +0800

    add HomePage component and update routing in App

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import LoginPage from "./pages/LoginPage";
 import MagicLinkSent from "./pages/MagicLinkSentPage";
 import "./App.css";
+import HomePage from "./pages/HomePage";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LoginPage />} />
         <Route path="/magic-link-sent" element={<MagicLinkSent />} />
+        <Route path="/home" element={<HomePage />} />
       </Routes>
     </BrowserRouter>
   );
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,9 @@
+export default function HomePage() {
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-green-50">
+      <h1 className="text-3xl font-bold text-green-600">
+        🎉 You’re logged in! Welcome to ThreadHub!
+      </h1>
+    </div>
+  );
+}
